82440f2d75c8d7d4c98d985b2cfb821479441075
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2023-09-07 21:27:50 +0300

    add soft delete

--- src/main/java/store/onlinebookstore/model/Order.java
+++ src/main/java/store/onlinebookstore/model/Order.java
@@ -16,10 +16,14 @@ import java.time.LocalDateTime;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
 @Entity
+@SQLDelete(sql = "UPDATE orders SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "orders")
 public class Order {
     @Id
--- src/main/java/store/onlinebookstore/model/OrderItem.java
+++ src/main/java/store/onlinebookstore/model/OrderItem.java
@@ -12,10 +12,14 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
 @Entity
+@SQLDelete(sql = "UPDATE order_items SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "order_items")
 public class OrderItem {
     @Id
